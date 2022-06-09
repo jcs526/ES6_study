868ad84fc040b3d795a1b18b578b73cf2ffef13c
ChanSol <mutongzang@gmail.com>
2022-06-09 20:55:03 +0900

    6/9 Vue로 웹게임 만들기

---
+++ public/vue_webgame/로또/LottoGenerator.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <div>당첨숫자</div>
+        <div id="결과창">
+            <lotto-ball v-for="ball in winBalls" :key="ball" :number="ball"></lotto-ball>
+        </div>
+        <div>보너스</div>
+        <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+        <button v-if="redo" @click="onClickRedo">한 번 더!!</button>
+    </div>
+</template>
+
+<script>
+import LottoBall from './LottoBall';
+
+function getWinNumbers() {
+    console.log('getWinNumbers');
+    const candiate = Array(45).fill().map((v,i)=>i+1);
+    const shuffle = [];
+    while (candiate.length>0){
+        shuffle.push(candiate.splice(Math.floor(Math.random()*candiate.length),1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length-1];
+    const winNumbers = shuffle.slice(0,6).sort((p,c)=>p-c);
+    return [...winNumbers,bonusNumber];
+}
+    const timeOuts = [];
+    export default{
+        components:{
+            'lotto-ball':LottoBall
+        },
+
+        data() {
+            return {
+                winBalls: [],
+                winNumbers: getWinNumbers(),
+                bonus:null,
+                redo:false,
+            };
+        },
+        computed: {
+          
+        },
+        methods: {
+            onClickRedo(){
+                this.winNumbers=getWinNumbers();
+                this.winBalls=[];
+                this.bonus=null;
+                this.redo=false;
+                this.showBalls();
+            },
+            
+        
+            showBalls(){
+               for(let i=0;i<(this.winNumbers.length-1);i++){
+                timeOuts[i] =setTimeout(()=>{
+                    this.winBalls.push(this.winNumbers[i]);
+                }, (i+1)*1000);
+            }
+            timeOuts[6]= setTimeout(()=>{
+                this.bonus=this.winNumbers[6];
+                this.redo=true;
+            },7000)
+            }
+
+        },
+          
+        watch:{
+
+        },
+
+        mounted() {
+            this.showBalls();
+        },
+        beforeDestroy() {
+            timeOuts.forEach((v)=>{
+                clearTimeout(v);
+                })
+        },
+    }
+</script>
+
+<style scoped>
+    #computer{
+        width: 142px;
+        height: 200px;
+        background-position: 0 0;
+    }
+  
+</style>
